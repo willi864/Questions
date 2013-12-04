48398bcb731b88d5aeb69b416f14634132ab8cac
li976 <li976@purdue.edu>
2013-12-03 23:00:13 -0500

    vision

--- HelpMe/gen/com/example/helpme/R.java
+++ HelpMe/gen/com/example/helpme/R.java
@@ -10,6 +10,11 @@ package com.example.helpme;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int blue=0x7f060000;
+        public static final int grey=0x7f060002;
+        public static final int white=0x7f060001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -22,25 +27,28 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int image=0x7f020001;
+        public static final int logo=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080010;
-        public static final int answer=0x7f08000b;
-        public static final int button1=0x7f080005;
-        public static final int button2=0x7f08000c;
-        public static final int button3=0x7f08000d;
-        public static final int button_ans=0x7f080003;
-        public static final int button_post=0x7f080008;
-        public static final int button_random=0x7f080001;
-        public static final int dialogButtonOK=0x7f08000f;
-        public static final int editText1=0x7f080004;
-        public static final int edit_answer=0x7f080002;
-        public static final int edit_message=0x7f080007;
-        public static final int edit_textanswer=0x7f08000a;
-        public static final int question=0x7f080009;
-        public static final int textView1=0x7f08000e;
-        public static final int text_post=0x7f080006;
-        public static final int text_question=0x7f080000;
+        public static final int action_settings=0x7f090011;
+        public static final int answer=0x7f09000b;
+        public static final int button1=0x7f090005;
+        public static final int button2=0x7f09000c;
+        public static final int button3=0x7f09000d;
+        public static final int button_ans=0x7f090003;
+        public static final int button_post=0x7f090008;
+        public static final int button_random=0x7f090001;
+        public static final int dialogButtonOK=0x7f090010;
+        public static final int editText1=0x7f090004;
+        public static final int edit_answer=0x7f090002;
+        public static final int edit_message=0x7f090007;
+        public static final int edit_textanswer=0x7f09000a;
+        public static final int frameLayout1=0x7f09000f;
+        public static final int question=0x7f090009;
+        public static final int textView1=0x7f09000e;
+        public static final int text_post=0x7f090006;
+        public static final int text_question=0x7f090000;
     }
     public static final class layout {
         public static final int activity_display_answer=0x7f030000;
@@ -51,11 +59,11 @@ public final class R {
         public static final int dialog=0x7f030005;
     }
     public static final class menu {
-        public static final int display_answer=0x7f070000;
-        public static final int display_look=0x7f070001;
-        public static final int display_post=0x7f070002;
-        public static final int display_select_question=0x7f070003;
-        public static final int main=0x7f070004;
+        public static final int display_answer=0x7f080000;
+        public static final int display_look=0x7f080001;
+        public static final int display_post=0x7f080002;
+        public static final int display_select_question=0x7f080003;
+        public static final int main=0x7f080004;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -101,10 +109,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
